b07eb53e30fa5cb30e38280f27cbc971cd0b5abf
Avi Singh <aviarya2006@gmail.com>
2026-09-17 19:33:37 +0530

    fix:Add Lombok @Getter and @Setter to model

--- src/main/java/com/example/notesapp/model/Note.java
+++ src/main/java/com/example/notesapp/model/Note.java
@@ -3,47 +3,32 @@ package com.example.notesapp.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Note {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    @NotBlank(message="Title should not be empty")
-    private String title;
-    @NotBlank(message="Content should not be empty")
-    private String content;
 
-    public Note() {
-    }
+    @NotBlank(message = "Title should not be empty")
+    private String title;
+
+    @NotBlank(message = "Content should not be empty")
+    private String content;
 
     public Note(String title, String content) {
         this.title = title;
-        this.content = content;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
         this.content = content;
     }
 }
