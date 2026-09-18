a31b8d9a1b79ae5ee2300fbc4ea1443ada39397e
Avi Singh <aviarya2006@gmail.com>
2026-09-18 14:40:51 +0530

    fix: image cleanup from S3 bucket on deletion

--- src/main/java/com/example/notesapp/service/S3Service.java
+++ src/main/java/com/example/notesapp/service/S3Service.java
@@ -1,15 +1,13 @@
 package com.example.notesapp.service;
 
-
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
@@ -19,32 +17,27 @@ import java.util.UUID;
 public class S3Service {
 
     private final S3Client s3Client;
-
-    @Value("${aws.s3.bucket-name}")
-    private String bucketName;
-
-    @Value("${aws.s3.region}")
-    private String region;
+    private final String bucketName;
+    private final String region;
 
     public S3Service(
-            @Value("${aws.access-key-id}") String accessKey,
-            @Value("${aws.secret-access-key}") String secretKey,
+            @Value("${aws.s3.bucket-name}") String bucketName,
             @Value("${aws.s3.region}") String region) {
 
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        this.bucketName = bucketName;
+        this.region = region;
         this.s3Client = S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
     public String uploadFile(MultipartFile file) {
-
         if (file == null || file.isEmpty()) {
             return null;
         }
 
-        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        String fileName = "notes/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
 
         try {
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
@@ -54,12 +47,22 @@ public class S3Service {
                     .build();
 
             s3Client.putObject(putObjectRequest,
-                    RequestBody.fromBytes(file.getBytes()));
-
+                    RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
 
             return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, fileName);
         } catch (IOException e) {
             throw new RuntimeException("Failed to upload image to S3", e);
         }
+    }
+
+    public void deleteFile(String imageUrl) {
+        if (imageUrl == null) {
+            return;
+        }
+        String key = imageUrl.substring(imageUrl.indexOf(".com/") + 5);
+        s3Client.deleteObject(DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build());
     }
 }
